c958cb9d29f1a40aabe813f324cb6b3f9882e8e6
suddonm <suddonm@gmail.com>
2017-11-14 17:36:10 -0800

    added nav button press to move

---
+++ Assets/Scripts/NavigationMenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NavigationMenuController : MonoBehaviour {
+
+	public class NavObject
+	{
+		public Transform Transform;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] targets = GameObject.FindGameObjectsWithTag("Destination");
+
+		
+
+		Button navButton = GetComponentInChildren<Button>();
+
+		Debug.Log("Found buttons: " + navButton.name);
+
+		GetComponentInChildren<Button>().onClick.AddListener(delegate { NavButtonClick(); });
+
+		//for (int i = 0; i < targets.Length; i++)
+		//{
+		//	GameObject button = new GameObject();
+		//	button.transform.parent = this.gameObject.transform;
+		//	button.AddComponent<RectTransform>();
+		//	button.AddComponent<Button>();
+		//	button.transform.position = Vector3.zero;
+		//}	
+	}
+	
+	public void NavButtonClick()
+	{
+		ShipController.SetDestination(GameObject.FindGameObjectWithTag("Destination").transform);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
